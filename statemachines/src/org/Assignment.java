611414759743f80e27f217ba2d06d71661699b37
AsiSkarp <usska18@student.sdu.dk>
2023-04-29 18:08:29 +0200

    Add reworked framework files

---
+++ statemachines/src/org/Assignment.java
@@ -0,0 +1,33 @@
+package org;
+
+public class Assignment {
+
+    private Object value;
+    private String name;
+    private boolean isGlobal = false;
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGlobal() {
+        return isGlobal;
+    }
+
+    public void setGlobal(boolean global) {
+        isGlobal = global;
+    }
+
+}
